4b05b6a5e19fa37c912eb51b43332d5029915514
hugoluiz1984 <57529085+hugoluiz1984@users.noreply.github.com>
2022-05-24 11:47:26 -0300

    Adicionando styled-components

---
+++ src/App.styles.ts
@@ -0,0 +1,12 @@
+
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Info = styled.div``;
+
+export const LogoLink = styled.a``;
+
+export const InfoArea = styled.div``;
+
+export const gridArea = styled.div``;
