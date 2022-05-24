9530f1aafe63c5888fc501b66cfd389220eb8de4
hugoluiz1984 <57529085+hugoluiz1984@users.noreply.github.com>
2022-05-24 13:42:22 -0300

    Adicionado Layout e Button

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,12 +1,52 @@
 
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100%;
+    max-width: 750px;
+    margin: auto;
+    display: flex;
+    padding: 55px 0;
 
-export const Info = styled.div``;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`;
 
-export const LogoLink = styled.a``;
+export const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: auto;
 
-export const InfoArea = styled.div``;
+    @media (max-width: 768px) {
+        margin-bottom: 50px;
+        align-items: center;
+    }
+`;
 
-export const gridArea = styled.div``;
+export const LogoLink = styled.a`
+    display: block;
+`;
+
+export const InfoArea = styled.div`
+    width: 100%;
+    margin: 10px 0;
+
+    @media (max-width: 768px) {
+        display: flex;
+        justify-content: space-around;
+        text-align: center;
+    }
+`;
+
+export const gridArea = styled.div`
+    flex:1;
+    display: flex;
+    justify-content: flex-end;
+
+    @media (max-width: 768px) {
+        
+        justify-content: center;
+        margin: 0 20px;
+    }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import * as C from './App.styles';
 import logoImage from './assets/logo.png';
+import RestartIcon from './svgs/restart.svg'
+import {InfoItem} from './components/InfoItem';
+import {Button} from './components/Button';
+
 
 const App = () => {
+
+  const resetAndCreateGrid = () => {
+
+  }
 
   return (
     <C.Container>
@@ -11,12 +19,14 @@ const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          ...
+          <InfoItem label='Tempo' value="00:00"/>
+          <InfoItem label='Movimentos' value="0"/>
         </C.InfoArea>
-        <button>Reiniciar</button>
+        <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid} />
+        
       </C.Info>
       <C.gridArea>
-
+        ...
       </C.gridArea>
     </C.Container>
   )
